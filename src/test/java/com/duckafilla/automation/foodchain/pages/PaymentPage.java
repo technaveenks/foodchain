64b3de7290652fea42929f025868c8d76ec92d20
divya uddaraju <92005051+divyau1024@users.noreply.github.com>
2021-12-04 23:14:02 +0530

    sample payment page

---
+++ src/test/java/com/duckafilla/automation/foodchain/pages/PaymentPage.java
@@ -0,0 +1,33 @@
+package com.duckafilla.automation.foodchain.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PaymentPage extends BasePage{
+	WebDriver driver;
+	
+	public PaymentPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver,this);
+	}
+		
+	@FindBy(id="twotabsearchtextbox")
+	private WebElement _txtSearchBox;
+	
+	@FindBy(id="nav-search-submit-button")
+	private WebElement _btnSearch;
+	
+	//driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iPhone");
+	public void enterValue() {
+		_txtSearchBox.sendKeys("iPhone");
+	}
+	
+	public SearchDetails clickOnSearchButton() {
+		_btnSearch.click();
+		return new SearchDetails();
+	}
+	
+
+}
